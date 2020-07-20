a4a39b3fc0fab5ac028cd86d49b367bfc1d5afd9
HwyToHell <holger.grzonka@web.de>
2020-07-20 22:39:50 +0200

    52 recipe-list template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  receipes: Recipe[] = [
+    new Recipe("pancake", "eggs, flour, milk", "https://unsplash.com/photos/ZgRTuPsmFQg")
+  ];
 
   constructor() { }
 
