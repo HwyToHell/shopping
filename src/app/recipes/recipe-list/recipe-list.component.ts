a743955cddee069364849fc0a1f441683f180a19
HwyToHell <holger.grzonka@web.de>
2020-07-27 23:23:49 +0200

    87 pass data to recipe-item - teacher solution

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -10,13 +10,21 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("pancake", "eggs, flour, milk",
     "https://images.unsplash.com/flagged/photo-1557609786-fd36193db6e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"),
-    new Recipe("pancake", "eggs, flour, milk",
-    "https://images.unsplash.com/flagged/photo-1557609786-fd36193db6e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"),
+    new Recipe("muffin", "flour, milk, baking soda, blueberries",
+    "https://images.unsplash.com/photo-1558303420-f814d8a590f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80"),
+    
   ];
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
   
   constructor() { }
 
   ngOnInit(): void {
+   // this.onSelected(0);
   }
 
+  onSelected = (item: Recipe) => {
+    console.log("recipe-list: " + item.name);
+    this.selectedRecipe.emit(item);
+  }
 }
