86d1ae0def38ad4abdd2e0bc1ce10326268d4e28
HwyToHell <holger.grzonka@web.de>
2020-09-05 20:19:33 +0200

    218 shopping list form with regex validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,18 +9,15 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild("nameInput")  nameInputRef: ElementRef;
-  @ViewChild("amountInput")  amountInputRef: ElementRef;
 
   constructor(private shoppingSvs: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(name, amount);
+  onAddItem(form: NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingSvs.addIngredient(newIngredient);
     console.log("onAddItem()");
   }
